ea006a597969041b1607bdb464bff0dc5684a26c
[nt2505] <77818979+nt2505@users.noreply.github.com>
2021-07-23 22:10:06 +0700

    demo PayPal use Node

---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const paypal = require('paypal-rest-sdk');
+const fs=require('fs');
+const exphdbs=require('express-handlebars');
+var path=require('path');
+const app = express();
+
+app.set('views',path.join(__dirname,"views"));
+app.engine('handlebars',exphdbs({defaultLayout:'main'}));
+app.set('view engine','handlebars');
+
+var items = [{
+    "name": "Head First JavaScript Programming: A Brain-Friendly Guide 1st Edition",
+    "sku": "01",
+    "price": "28.00",
+    "currency": "USD",
+    "quantity": 2
+},
+{
+    "name": "Head First Python: A Brain-Friendly Guide 2nd Edition",
+    "sku": "02",
+    "price": "60.00",
+    "currency": "USD",
+    "quantity": 1
+},
+{
+    "name": "Learn Python 3 the Hard Way",
+    "sku": "03",
+    "price": "17.00",
+    "currency": "USD",
+    "quantity": 1
+}]
+
+var total = 0;
+for (let i=0; i<items.length;i++){
+    total += parseFloat(items[i].price)*items[i].quantity;
+}
+
+// Config PayPal
+paypal.configure({
+    'mode': 'sandbox',
+    'client_id': 'ATY9aOyCSuntd63fC9KQJII4cN2IrkpfdCtvRy803sWo3VwTIfbnYsVQ05voqsSHYkCOND2US-7QG9LL',
+    'client_secret': 'ELybaHiA4KOZjLPDOJs_ZCBt0q2sDEw6qAMQVduHAKVMtTK9lMpy-Aj9aR15dvZ2hVbMvDgRRlRelGsF'
+})
+
+// Trang chu
+app.get('/',function(req,res){
+    res.render('index.handlebars',{"items":items});
+})
+
+app.post('/pay', function(req,res){
+    const create_payment_json = {
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/success", //khi thanh toan thanh cong
+            "cancel_url": "http://localhost:3000/cancel" // khi thanh oan that bai
+        },
+        "transactions": [{
+            "item_list": {
+                "items": items
+            },
+            "amount": {
+                "currency": "USD",
+                "total": total.toString()
+            },
+            "description": "Demo PayPal SandBox"
+        }]
+    };
+
+    // Tao payment
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            for (let i = 0; i<payment.links.length;i++)
+            {
+                if (payment.links[i].rel === 'approval_url')
+                {
+                    res.redirect(payment.links[i].href);
+                    console.log(payment);
+                }
+            }
+        }
+    });
+    
+})
+
+app.get('/success', function(req,res){
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+
+    var execute_payment_json = {
+        "payer_id": payerId,
+        "transactions": [{
+            "amount": {
+                "currency": "USD",
+                "total": total.toString()
+            }
+        }]
+    };
+
+    // Thuc hien payment
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+        if (error) {
+            res.render('fail.handlebars')
+        } else {
+            res.render('success.handlebars');  
+            console.log(payment);
+        }
+});
+});
+
+app.listen(3000, function(){
+    console.log('Listening');
+});
